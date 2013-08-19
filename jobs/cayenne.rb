2cef5dc47703ee2c843d47ab330265cd086ee366
Karl Jonathan Ward <karl.j.ward@gmail.com>
2013-08-19 16:12:53 +0100

    Start of cayenne dashboard. Hits cayenne's /metrics.

---
+++ jobs/cayenne.rb
@@ -0,0 +1,32 @@
+require 'json'
+require 'net/http'
+
+SCHEDULER.every '5s', :first_in => 0 do |job|
+  begin
+    uri = URI.parse('http://localhost:3000/metrics')
+    metrics = JSON.parse(Net::HTTP.get(uri))
+
+    metrics.each_pair do |name, metric_info|
+      case metric_info['type']
+      when 'meter'
+        send_event(name, {current: metric_info['rates']['1']})
+      when 'gauge'
+        send_event(name, {current: metric_info['current']})
+      when 'histogram'
+        send_event(name + '.mean', {current: metric_info['mean']})
+        send_event(name + '.min', {current: metric_info['min']})
+        send_event(name + '.max', {current: metric_info['max']})
+        send_event(name + '.sd', {current: metric_info['standard-deviation']})
+      when 'timer'
+        send_event(name + '.mean', {current: metric_info['mean']})
+        send_event(name + '.min', {current: metric_info['min']})
+        send_event(name + '.max', {current: metric_info['max']})
+        send_event(name + '.sd', {current: metric_info['standard-deviation']})
+        send_event(name, {current: metric_info['rates']['1']})
+      end
+    end
+  rescue StandardError => e
+    puts "Failed to get metrics data."
+    puts e
+  end
+end
